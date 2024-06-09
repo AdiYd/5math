22557241994fedc783717ec7a0495ca1cb0d22cd
AdiY <YdDevelops@gmail.com>
2024-06-09 17:12:05 +0300

    Additinal files changes and re-order

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function Home() {
+    return (
+        <div className="App">
+            <h1>This is the start234234ing point!</h1>
+        </div>
+    );
+}
+
+export default Home;
