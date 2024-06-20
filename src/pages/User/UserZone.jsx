66bc444b277792508c1f3f322093bdbb12257a35
AdiY <YdDevelops@gmail.com>
2024-06-20 19:22:10 +0300

    commit all changes

--- src/pages/User/UserZone.jsx
+++ src/pages/User/UserZone.jsx
@@ -1,9 +1,10 @@
 import './User.css';
 import { User } from '../..';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import PrincipleDoc from '../../assets/Documents/PrincipleDoc.docx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import wordIcon from './wordLogo.svg';
+import { useNavigate } from 'react-router-dom';
 
 
 function onClickHandler(e) {
@@ -12,19 +13,25 @@ function onClickHandler(e) {
 
 function UserZone({ ...props }) {
     const user = useContext(User);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!user.isAuth) {
+            navigate('/Home')
+        }
+    }, [user, user.isAuth])
     return (
         <div className='UserZone'>
             <div className='flex center'>
                 <h2> שלום {user.name} </h2>
             </div>
 
-            <a href={PrincipleDoc} download="PrincipleDoc" className='flex center opacityHover gap1 pointer'>
+            {user.isAdmin && <a href={PrincipleDoc} download="PrincipleDoc" className='flex center opacityHover gap1 pointer'>
                 <img src={wordIcon}
                     style={{ width: '1em', height: '1em' }}
                     title='מסמך עקרונות' alt='Word document Icon' />
                 לחץ להורדת מסמך עקרונות
-            </a>
-
+            </a>}
+            {[1, 2, 3]}
 
         </div>
     )
