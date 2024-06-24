adcb054ee1a7f67b74892afbd0f721890e95a704
AdiY <YdDevelops@gmail.com>
2024-06-24 20:41:09 +0300

    adding promo video to main page

---
+++ src/components/YoutubeEmb.jsx
@@ -0,0 +1,34 @@
+import {useState, useEffect}  from "react";
+import useWindowDimensions from "../assets/function/useWindowDimentions";
+
+const YoutubeEmb = ({ 
+    height = '90%',
+    width= '90%',
+    frameBorder=0,
+    borderRadius=10,
+    title = 'Video',
+    embedId = '-sxfriO_IV0'
+        }) => {
+            const {widthS, heightS} = useWindowDimensions();
+            const [update, setUpdate] = useState(false);
+            useEffect(()=>{
+                setUpdate(p=>!p);
+            },[widthS, heightS])
+
+            return(
+            <div className="video-responsive alignCenter">
+            <iframe
+                style={{borderRadius:`${borderRadius}px`}}
+                width={width}
+                height={height}
+                src={`https://www.youtube.com/embed/${embedId}`}
+                // frameBorder={`${frameBorder}`} 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerPolicy="strict-origin-when-cross-origin" 
+                allowFullScreen
+                title={title}
+            />
+            </div>)
+        };
+
+export default YoutubeEmb;
